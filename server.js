0e4678f5221fdf2092fd332fe6507f084bc9f4dd
jambrose0 <jambrose0518@gmail.com>
2022-03-15 21:00:48 -0400

    completed routes for front-end

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const { animals } = require("./data/animals");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -100,6 +101,24 @@ app.post("/api/animals", (req, res) => {
   }
 });
 
+app.get("/animals", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+app.get("/zookeepers", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/zookepers.html"));
+});
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+app.get("/animals", (rep, res) => {
+  res.sendFile(path.join(__dirname, "./public/animals.html"));
+});
+app.get("/zookeepers", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/zookeepers.html"));
+});
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
